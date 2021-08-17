57056d374e76cf15ad3aa9ee70dc5bc4b1eeacb0
IudexSoren <aaronsibajam@gmail.com>
2021-08-17 17:18:14 -0500

    Mensaje

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button 
+        class="btn" 
+        :style="{ background: color }"
+        @click="onClick()"
+    >
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: {
+            type: String
+        },
+        color: {
+            type: String
+        }
+    },
+    methods:{
+        onClick() {
+            this.$emit('btn-click');
+        }
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button 
+            :text="showAddTask ? 'Close' : 'Add Task' " 
+            :color="showAddTask ? 'red' : 'green' " 
+            @btn-click="$emit('toggle-add-task')" 
+        />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String
+        },
+        showAddTask: {
+            type: Boolean
+        }
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1.5em;
+    }
+</style>
